6864e3e85a9f73b69e69073bd122b6f20f2f3be3
Jason Nowell <jnowell@ufl.edu>
2024-07-14 16:59:33 -0400

    Still trying to get workingtrees to ... work.

--- xronosVr/firstOrder/figures/figure-crop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cp -f $1 $1.bak
-pdfcrop $1 .foo.pdf && cp .foo.pdf $1 && rm -f .foo.pdf
--- xronosVr/fourierTrans/figures/optimize-svgs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-echo OPTIMIZING SVG...
-for n in *.svg ; do
-  echo svgo --disable=convertPathData --multipass $n
-  svgo --disable=convertPathData --multipass $n
-done
